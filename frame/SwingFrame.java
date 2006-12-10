6272e28bd1d4508e1fb8d7a827ea99a041139b36
zenjiro <zenjiro0123@gmail.com>
2006-12-10 07:29:38 +0000

    Swingを使ったフレームを作ってみました。

---
+++ frame/SwingFrame.java
@@ -0,0 +1,17 @@
+package frame;
+
+import javax.swing.JFrame;
+
+/**
+ * Swingのフレームです。
+ * @author zenjiro
+ */
+public class SwingFrame extends JFrame {
+	/**
+	 * コンストラクタです。
+	 */
+	public SwingFrame() {
+		super("わーい");
+		super.setSize(400, 300);
+	}
+}
