c90c30a9c7d9c5c1ddb05d9c4b674798ccab61d2
zenjiro <zenjiro0123@gmail.com>
2006-12-10 07:37:34 +0000

    いろいろ削除しました。

--- Main.java
+++ Main.java
@@ -1,16 +1,9 @@
 
 import icon.ChatIcon;
 import icon.ChatIconListener;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.StringTokenizer;
-import network.Network;
-import network.NetworkListener;
-import frame.MainFrameListener;
-import frame.MainFrame;
 
 /**
  * UDPマルチキャストを用いたチャットクライアントです。
@@ -19,19 +12,9 @@ import frame.MainFrame;
  */
 public class Main {
     /**
-     * 最後に会話が行われた時刻
-     */
-    static long lastTalkingTime;
-
-    /**
-     * アイコンを誰も話していない状態に戻すまでの待ち時間
-     */
-    public static final int TIME_TO_NO_ONE_TALKING = 10000;
-
-    /**
      * メインメソッドです。
-     * 実行するには、以下のVMオプションをつけて下さい。
-     * -Djava.library.path=/usr/local/eclipse3/eclipse/plugins/org.eclipse.swt.gtk_3.1.0/os/linux/x86/
+     * 実行するには、以下のコマンドを実行してください。
+     * java -classpath swt-M20060921-0945-gtk-linux-x86/swt.jar:. -Djava.library.path=swt-M20060921-0945-gtk-linux-x86/ Main
      * @param args コマンドライン引数
      * @throws UnknownHostException
      */
@@ -50,114 +33,12 @@ public class Main {
                 }
             }
         }
-        final String name = userName;
-        final Network network = new Network(name);
-        final MainFrame frame = new MainFrame(name, defaultStatus);
         final ChatIcon icon = new ChatIcon();
-        network.setNetworkListener(new NetworkListener() {
-            public void messageReceived(String message) {
-                Calendar calender = Calendar.getInstance();
-                DateFormat format = new SimpleDateFormat("HH:mm");
-                //frame.append(format.format(calender.getTime()) + "\t" + message); 
-                frame.append(message);
-                StringTokenizer tokeinizer = new StringTokenizer(message);
-                if (tokeinizer.countTokens() > 1) {
-                    if (!tokeinizer.nextToken().equals(name)) {
-                        lastTalkingTime = System.currentTimeMillis();
-                        if (message.indexOf(name) > 0) {
-                            icon.setIcon(ChatIcon.ICON_CALLED_ME);
-                        } else {
-                            if (icon.getIcon() == ChatIcon.ICON_NO_ONE_TALKING) {
-                                icon.setIcon(ChatIcon.ICON_OTHERS_TALKING);
-                            }
-                        }
-                    }
-                }
-            }
-
-            public void statusReceived(String user, String status, boolean isOpen) {
-                frame.setStatus(user, status, isOpen);
-            }
-        });
-        frame.setFrameListener(new MainFrameListener() {
-            public void messageSent(String message) {
-                if (message.length() > 0) {
-                    network.sendMessage(message);
-                }
-            }
-
-            public void statusChanged() {
-                network.sendStatus(frame.getStatus(), frame.isOpen());
-            }
-
-            public void appeared() {
-                network.sendStatus(frame.getStatus(), frame.isOpen());
-            }
-
-            public void disappeared() {
-                network.sendStatus(frame.getStatus(), frame.isOpen());
-            }
-        });
         icon.setListener(new ChatIconListener() {
             public void clicked() {
-                if (frame.isOpen()) {
-                    frame.hide();
-                } else {
-                    frame.show();
-                    if (icon.getIcon() == ChatIcon.ICON_CALLED_ME) {
-                        if (System.currentTimeMillis() - lastTalkingTime > TIME_TO_NO_ONE_TALKING) {
-                            icon.setIcon(ChatIcon.ICON_NO_ONE_TALKING);
-                        } else {
-                            icon.setIcon(ChatIcon.ICON_OTHERS_TALKING);
-                        }
-                    }
-                }
-            }
-        });
-        new SendStatusThread(frame, network).start();
-        new ChangeIconThread(icon).start();
-        network.connect();
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            public void run() {
-                network.sendStatus(null, false);
-                network.disconnect();
+            	System.out.println("clicked");
             }
         });
         icon.waitForDisposed();
-    }
-
-    /**
-     * しばらく会話がないときに、アイコンを戻すクラスです。
-     * @author zenjiro
-     * 作成日：2004/11/12
-     */
-    static class ChangeIconThread extends Thread {
-        /**
-         * ネットワーク
-         */
-        private ChatIcon icon;
-
-        /**
-         * コンストラクタです。
-         * @param icon アイコン
-         */
-        public ChangeIconThread(ChatIcon icon) {
-            this.icon = icon;
-        }
-
-        public void run() {
-            while (true) {
-                try {
-                    Thread.sleep(1000);
-                    if (System.currentTimeMillis() - lastTalkingTime > TIME_TO_NO_ONE_TALKING) {
-                        if (this.icon.getIcon() == ChatIcon.ICON_OTHERS_TALKING) {
-                            this.icon.setIcon(ChatIcon.ICON_NO_ONE_TALKING);
-                        }
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 }
